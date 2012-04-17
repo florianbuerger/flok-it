a4d01cb2e7d4b7c20a6cdc4e55b604f6db903894
Florian Buerger <florian@florianbuerger.com>
2012-04-17 15:32:04 +0200

    Added new AssertLog
    inspired by Marcus Zarra (http://www.cimgf.com/2010/05/02/my-current-prefix-pch-file/)
    
    Usage:
    
    FBAssert(…)
    - will crash in DEBUG mode when condition is not met
    - will log error in PRODUCTION mode when condition is not met (==> requires cocoa-lumberjack)

--- flok-it.h
+++ flok-it.h
@@ -33,6 +33,18 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 static const int ddLogLevel = LOG_LEVEL_WARN;
 #endif
 
+#ifdef DEBUG
+// assertions - thanks to m. zarra - http://www.cimgf.com/2010/05/02/my-current-prefix-pch-file/
+#define AssertLog(...) [[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] lineNumber:__LINE__ description:__VA_ARGS__]
+#else
+#ifndef NS_BLOCK_ASSERTIONS
+    #define NS_BLOCK_ASSERTIONS
+#endif
+#define AssertLog(...) DDLogError(__VA_ARGS__)
+#endif
+
+#define FBAssert(condition, ...) do { if (!(condition)) { AssertLog(__VA_ARGS__); }} while(0)
+
 #import "UIApplication+NetworkIndicator.h"
 
 #import "FBBaseViewController.h"
